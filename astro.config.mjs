12bac14493d9d2f8ab1098b259ef6e384c73b90e
tduforet <thomas.duforet@icloud.com>
2024-11-03 07:18:47 +0100

    end of astro blog tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import {defineConfig} from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://thomasduforet.fr',
+  site: 'https://thomasduforet.fr',
+  integrations: [react()],
 });
