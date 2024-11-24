71fa3ec2dddfabfe5a59aa6504c3d36c790e85ce
tduforet <thomas.duforet@icloud.com>
2024-11-24 19:04:36 +0100

    add reading time

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import {defineConfig} from 'astro/config';
-
+import {remarkReadingTime} from './src/scripts/remark-reading-time.js';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://thomasduforet.fr',
-  integrations: [react()],
+    site: 'https://thomasduforet.fr',
+    integrations: [react()],
+    markdown: {
+        remarkPlugins: [remarkReadingTime]
+    }
 });
---
+++ src/scripts/remark-reading-time.js
@@ -0,0 +1,11 @@
+import { readingTime } from 'reading-time-estimator'
+import { toString } from 'mdast-util-to-string';
+
+export function remarkReadingTime() {
+    return function (tree, { data }) {
+        const textOnPage = toString(tree);
+        const result = readingTime(textOnPage, 260, 'fr')
+
+        data.astro.frontmatter.minutesRead = result.text;
+    };
+}
